24379543ef7037d9ab2308b397add7de68fa02b6
Phil Chodrow <pchodrow@gmail.com>
2023-03-10 16:46:26 -0500

    fixes and updates

--- posts/example-blog-post/source.py
+++ posts/example-blog-post/source.py
@@ -1,2 +1,10 @@
 def my_custom_function():
     print("I implemented this function in the file " + __name__ + ".py")
+    
+class Perceptron:
+    
+    def __init__(self):
+        print("I did it!!")
+    
+    def fit(X, y, self):
+        print("not implemented")
