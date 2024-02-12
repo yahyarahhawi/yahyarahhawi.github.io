52099b5b03afe2b953d05f390048d5038e981199
Philip Chodrow <pchodrow@gmail.com>
2024-02-12 13:57:01 -0500

    remove stray lecture notes from template

--- posts/example-blog-post/source.py
+++ posts/example-blog-post/source.py
@@ -1,10 +1,3 @@
 def my_custom_function():
     print("I implemented this function in the file " + __name__ + ".py")
     
-class Perceptron:
-    
-    def __init__(self):
-        print("I did it!!")
-    
-    def fit(X, y, self):
-        print("not implemented")
